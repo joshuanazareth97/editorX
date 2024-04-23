65808b5eff71a4a108c09f0b3094b73acbfca0fa
Joshua <joshuanazareth97@gmail.com>
2024-04-23 09:53:50 +0530

    Add auth API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+// Auth Routes
+export const apiLogin = async (username) => {
+  return await axios.post("/api/user/login", { username });
+};
+
+export const apiLogout = async () => {
+  return await axios.post("/api/user/logout");
+};
+
+// Document Routes
+
 export const createDocumentMeta = async () => {
   return await axios.post("/api/document");
 };
