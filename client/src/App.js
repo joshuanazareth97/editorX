ab56e1dd41d80d27bfbdd78faf9040a05380a799
Joshua <joshuanazareth97@gmail.com>
2024-04-23 09:53:50 +0530

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./404";
+import "./App.css";
+import Editor from "./components/Editor/Editor";
+import FileListLayout from "./layouts/FileListLayout";
+import { theme } from "./theme";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FileListLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/:documentId",
+        element: <Editor />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
